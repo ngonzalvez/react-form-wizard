58911f1a30853e946c3a05c424e4f0d2d1df3918
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2024-01-09 00:53:21 -0300

    CheckboxField: Fix logic

--- src/CheckboxField.tsx
+++ src/CheckboxField.tsx
@@ -2,15 +2,15 @@ import { FC, useCallback, useState } from "react";
 import styles from "./CheckboxField.module.scss";
 
 interface CheckboxFieldProps {
-  key: string;
-  options: string[];
+  name: string;
+  options: string[] | Array<{ value: string; label: string }>;
   onChange: (values: Record<string, boolean>) => any;
 }
 
 /**
  * A checkbox field.
  */
-const CheckboxField: FC<CheckboxFieldProps> = ({ key, options, onChange }) => {
+const CheckboxField: FC<CheckboxFieldProps> = ({ name, options, onChange }) => {
   //--------------------------------------------------------------------------------------------------------------------
   //                                                       STATE
   //--------------------------------------------------------------------------------------------------------------------
@@ -23,8 +23,10 @@ const CheckboxField: FC<CheckboxFieldProps> = ({ key, options, onChange }) => {
   //--------------------------------------------------------------------------------------------------------------------
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [e.target.name]: e.target.checked });
-      onChange(values);
+      const newValue = { ...values, [e.target.name]: e.target.checked };
+      setValues(newValue);
+      onChange(newValue);
+      console.log("Handle checkbox change", newValue);
     },
     [onChange]
   );
@@ -35,8 +37,14 @@ const CheckboxField: FC<CheckboxFieldProps> = ({ key, options, onChange }) => {
   return (
     <div className={styles.CheckboxField}>
       {options.map((option) => (
-        <label className={styles.option} key={key + "_" + option.value}>
-          <input type="checkbox" name={key} checked={option.checked} className={styles.input} onChange={handleChange} />
+        <label className={styles.option} key={name + "_" + option.value}>
+          <input
+            type="checkbox"
+            name={option.value}
+            checked={option.checked}
+            className={styles.input}
+            onChange={handleChange}
+          />
           <span className={styles.checkmark}></span>
           {option.label || option}
         </label>
